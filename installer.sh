d13d233c55c0424bdac1ed8b20e29cb428437cf0
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-02-11 01:58:11 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,13 +2,13 @@
 #!/bin/sh
 #
 
-wget -O /var/volatile/tmp/Skin-EUR02020-FHD-N-By-Muaath.ipk "https://drive.google.com/uc?id=1CBTK9_xu1rNrTaBL1JIk4qGNBDViuoHl&export=download"
-
+wget -O /var/volatile/tmp/Skin-EUR02020-FHD-N-By-Muaath.ipk 
+"https://drive.google.com/uc?id=1CBTK9_xu1rNrTaBL1JIk4qGNBDViuoHl&export=download"
 echo ""
 # Download and install plugin
 cd /tmp
 set -e
-echo "===> Downloading And Installing  Skin-EUR02020-FHD-N-By-Muaath.ipk Please Wait ......"
+echo "===> Downloading And Installing  Skin-EUR02020.ipk Please Wait ......"
 echo
 opkg install --force-overwrite /var/volatile/tmp/*.ipk
 set +e
