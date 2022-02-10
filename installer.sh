7cfe5ea4f391a23f5c0f03c106c0467063b6c131
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-02-11 01:40:13 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,29 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/Skin-EUR02020-FHD-N-By-Muaath.ipk "https://drive.google.com/uc?id=1CBTK9_xu1rNrTaBL1JIk4qGNBDViuoHl&export=download"
+
+echo ""
+# Download and install plugin
+cd /tmp
+set -e
+echo "===> Downloading And Installing  Skin-EUR02020 Please Wait ......"
+echo
+opkg install --force-overwrite /tmp/*.ipk
+set +e
+rm -f docsat.tar.gz
+
+echo ""
+sync
+echo "##############################################################"
+echo "#         Skin-EUR02020 INSTALLED SUCCESSFULLY             #"
+echo "#             UPLOADED BY TAREK                #"
+echo "##############################################################"
+echo "**************************************************************"
+echo "##############################################################"
+echo "#              RESTART YOUR STB NOW                  #"
+echo "##############################################################"
+
+exit 0
+
